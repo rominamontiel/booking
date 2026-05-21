12c6cda35516e6ff892557b27eb161436e1e5640
Romina Montiel <romina.montiel@fluxit.com.ar>
2026-05-21 20:38:08 -0300

    Auto-generated commit

---
+++ chunk-54KT2GV3.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var o=[{path:"",loadComponent:()=>import("./chunk-57H5YLN5.js")},{path:":id",loadComponent:()=>import("./chunk-3TUSR4JM.js")}];export{o as BOOKING_ROUTES};
